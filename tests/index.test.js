f1fd7243d4080a46de0cab5b02fd066c4ed0c641
KeisukeYamashita <19yamashita15@gmail.com>
2019-06-16 17:22:40 +0900

    :zap: add test

--- tests/index.test.js
+++ tests/index.test.js
@@ -2,7 +2,7 @@
 
 const publisher = require('../index.js');
 
-test('set properties correctly', () => {
+test('Publisher() => set properties correctly', () => {
     const projectId = "testProjectID";
     const topicName = "myKekeTopic";
     const publisherClient = new publisher.Publisher(projectId, topicName);
@@ -10,3 +10,37 @@ test('set properties correctly', () => {
     expect(publisherClient.projectId).toBe(projectId);
     expect(publisherClient.topicName).toBe(topicName);
   });
+  
+test('createData() => set fields correctly without Option', () => {
+  const type = 'text';
+  const from = 'keke';
+  const message = 'me';
+
+  expect(publisher.createData(type, from, message)).toStrictEqual({
+    type: type,
+    payload: {
+      from: from,
+      message: message
+    }
+  })
+})
+
+  
+test('createData() => set fields correctly with Option', () => {
+  const type = 'text';
+  const from = 'keke';
+  const message = 'me';
+  const option = {
+    title: "Hello",
+    value: "Is me~~"
+  };
+
+  expect(publisher.createData(type, from, message, option)).toStrictEqual({
+    type: type,
+    payload: {
+      from: from,
+      message: message,
+      option: option
+    }
+  })
+})
